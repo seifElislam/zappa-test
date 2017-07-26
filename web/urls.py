102035e357d8ee93180d425359b2dd06c2e7835f
Seif <seif.eleslam1990@gmail.com>
2017-07-26 15:19:23 +0200

    add functions view + urls

--- web/urls.py
+++ web/urls.py
@@ -2,7 +2,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-
-    url(r'^login/$', views.login, name='login'),
+     url(r'^login/$', views.login, name='login'),
+     url(r'^messages/$', views.message_list),
+     url(r'^messages/(?P<pk>[0-9]+)$', views.snippet_detail),
 
 ]
--- web/views.py
+++ web/views.py
@@ -4,6 +4,11 @@ from django.contrib.auth import authenticate
 from django.shortcuts import render
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from web.models import Message
+from web.serializers import MessageSerializer
 
 
 @csrf_exempt
@@ -20,4 +25,48 @@ def login(request):
         else:
             print(403)
             return JsonResponse({'msg': 'not allowed'}, status=403)
+
+
+@api_view(['GET', 'POST'])
+def message_list(request):
+    """
+    List all messages, or create a new message.
+    """
+    if request.method == 'GET':
+        snippets = Message.objects.all()
+        serializer = MessageSerializer(snippets, many=True)
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        serializer = MessageSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def snippet_detail(request, pk):
+    """
+    Retrieve, update or delete a message instance.
+    """
+    try:
+        snippet = Message.objects.get(pk=pk)
+    except Message.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = MessageSerializer(snippet)
+        return Response(serializer.data)
+
+    elif request.method == 'PUT':
+        serializer = MessageSerializer(snippet, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        snippet.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
